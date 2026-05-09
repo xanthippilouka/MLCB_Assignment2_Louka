ff5688d5eb0f1e477049b58e9107dae4d2f2e318
xanthippilouka <xanthiluka@gmail.com>
2026-05-10 01:53:36 +0300

    Finalize the nCV Class, train the baseline models with default hyperparameters, define the hyperparameters spaces

--- src/nested_cross_validation.py
+++ src/nested_cross_validation.py
@@ -12,11 +12,12 @@ from sklearn.base import clone
 from sklearn.model_selection import GridSearchCV, RepeatedStratifiedKFold, cross_val_score, StratifiedKFold
 from sklearn.metrics import (matthews_corrcoef, roc_auc_score, balanced_accuracy_score,f1_score, recall_score, precision_score, average_precision_score, confusion_matrix)
 import optuna
+import joblib
 
 
 class NestedCrossValidation:
     #I will use this class for both baseline comparison (with default hyperparameters) and for hyperparameters tuning (Optuna)
-    #I will set this class to work on two modes - optimize=False no inner loop (training on the outer loop) and optimize=True (inner loop for Optuna for hyperparameters tuning and evaluation on the outer loop)
+    #I will set this class to work on two modes - optimize=False no inner loop (training on the outer loop) and optimize=True (inner loop for Optuna for hyperparameters tuning)
     def __init__(self, data, estimators, parameter_space, R=10, n_outer=5, n_inner=3, seed=42, optimize=False):
 
         self.df = data
@@ -33,7 +34,7 @@ class NestedCrossValidation:
         self.seed = seed
         self.optimize = optimize
 
-        self.X = data
+        self.X = data.drop(columns=[self.target])
         self.y = data[self.target]
 
         self.results = {}
@@ -52,7 +53,7 @@ class NestedCrossValidation:
 
     def runcv(self):
         
-        #Use RepeatedStratifiesKFold to ensure proper class imbalance handling
+        #Use RepeatedStratifiedKFold to ensure proper class imbalance handling
         outer_cv = RepeatedStratifiedKFold(n_splits=self.n_outer, n_repeats=self.n_rounds, random_state=self.seed) #Outer folds
 
         for name, model in self.estimators:
@@ -67,7 +68,7 @@ class NestedCrossValidation:
 
                 if should_optimize and (name in self.param_space):
                     #Enter the Inner loop for Optuna tuning
-                    best_params = self.optuna(X_train, y_train, clone(model), self.param_space[name])
+                    best_params = self.optuna(X_train, y_train, clone(model), self.param_space[name]) #clone : create a new, unfitted version of the model 
                     model.set_params(**best_params)
                 
                 #If not enter the inner loop for optimization, training and evaluation on the outer loop
@@ -87,7 +88,7 @@ class NestedCrossValidation:
 
                 fold_metrics.append(self.calculate_metrics(y_test, y_pred, y_proba))
 
-        self.results[name] = pd.DataFrame(fold_metrics) #At the end of one round I have 5 sets of the metrics for each model. After the 10 rounds, I have 50 sets of metrics
+            self.results[name] = pd.DataFrame(fold_metrics) #At the end of one round I have 5 sets of the metrics for each model. After the 10 rounds, I have 50 sets of metrics
 
 
     def optuna(self, X_train_outer, y_train_outer, estimator, param_space):
@@ -112,7 +113,7 @@ class NestedCrossValidation:
 
             return np.mean(scores)
 
-        study = optuna.create_study(direction="maximize")
+        study = optuna.create_study(direction="maximize", sampler =optuna.samplers.TPESampler(seed=self.seed)) #make the sampler behave in a deterministic way
         study.optimize(objective, n_trials=50)
         return study.best_params
 
@@ -131,8 +132,19 @@ class NestedCrossValidation:
             'PRAUC' : average_precision_score(y_test, y_proba) if y_proba is not None else np.nan,
             'Specificity': specificity
             }
+    
+    def save_model(self, model_name, X_full, y_full, filename):
+        #Save the model with the best hyperparameters if tuned or baseline models
+        for name, model in self.estimators:
+            if name==model_name:
+                #Save thee full pipeline not just the model (mathemtics)
+                full_pipe = self.preprocessing_pipeline(model)
+                full_pipe.fit(X_full, y_full) #Fit on the entire development set, only the winner algorithm
+                #Save the full pipeline
+                joblib.dump(full_pipe, filename)
+                print(f"Complete pipeline for {model_name} saved successfully")
+
        
-                
 #Function for Bootstrap
 def bootstrap(results_df, n_resamples=1000, confidence_level=0.95, seed=42):
     np.random.seed(seed) #starting point
@@ -171,9 +183,44 @@ def bootstrap(results_df, n_resamples=1000, confidence_level=0.95, seed=42):
     return pd.DataFrame(summary_stats)
 
 
+#Generate a function to save the results from the models after bootstraping
+def get_models_statistics(results_dict, output_filename="stats_report.csv"):
+    all_sum = []
+
+    for model_name, df in results_dict.items():
+        summary = bootstrap(df)
+        summary['Model'] = model_name
+        all_sum.append(summary)
+
+    #Save the report
+    report_df = pd.concat(all_sum).reset_index(drop=True)
+    report_df.to_csv(output_filename, index=False)
+
+    return report_df
 
 
-   
+#Function to plot metrics
+def plot_metrics(stats_df, metrics = ['MCC', 'Recall', 'AUC'], filename="comparison_metrics.png"):
 
-          
-    return pd.DataFrame(stats, columns=['RMSE', 'MAE', 'R2', 'Pearson_r'])
+    plt.figure(figsize=(15,5))
+
+    for i, metric in enumerate(metrics):
+        plt.subplot(1, len(metrics), i+1)
+        data = stats_df[stats_df['Metric']==metric]
+
+        #Median and 95% intervals
+        plt.errorbar(
+            x=data['Model'],
+            y=data['Median'],
+            yerr=[data['Median'] - data['CI_Lower'], data['CI_Upper'] - data['Median']],
+            fmt = 'o', color='teal', capsize=5, capthick=2
+        )
+        plt.title(f"Median {metric} (95% CI)", fontsize=12)
+        plt.xticks(rotation=45, ha='right', rotation_mode='anchor')
+        plt.ylabel('Score')
+        plt.grid(axis='y', linestyle='--', alpha=0.7)
+
+    plt.tight_layout(pad=3.0)
+    plt.savefig(filename, dpi=300, bbox_inches='tight')
+    plt.show()
+
